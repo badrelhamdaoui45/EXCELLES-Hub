4dc08a32d132c5a424a010d8d9ab3bc8bcb3c017
badrelhamdaoui45 <151945409+badrelhamdaoui45@users.noreply.github.com>
2025-09-18 13:29:35 +0200

    Update layout.tsx add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17542032820"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17542032820');
+</script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&family=Cinzel:wght@400..900&display=swap" rel="stylesheet" />
