b9d3c56185521743b6d6b0a99374b6e4fe1154ea
badr elhamdaoui <badrelhamdaoui45@gmail.com>
2025-09-18 11:59:38 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,17 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17542032820"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
         
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17542032820"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17542032820');  
-</script>
-
+          gtag('config', 'AW-17542032820');
+        `}} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&family=Cinzel:wght@400..900&display=swap" rel="stylesheet" />
